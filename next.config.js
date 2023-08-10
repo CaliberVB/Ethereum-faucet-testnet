ab3311fbe765229d28f09875ad5747882e5f8e9f
Phathdt <phathdt379@gmail.com>
2023-08-10 15:55:26 +0700

    format code

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
-         config.resolve.fallback.fs = false
-         config.resolve.fallback.dns = false
-         config.resolve.fallback.net = false
-         config.resolve.fallback.tls = false
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.dns = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
     }
 
     return config;
-}
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
