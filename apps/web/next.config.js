f8276be95b109631fbc4561e1f5a5a30237e497e
Phathdt <phathdt379@gmail.com>
2023-08-12 00:22:16 +0700

    update folder

---
+++ apps/web/next.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNx = require('@nx/next/plugins/with-nx');
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withNx(nextConfig);
