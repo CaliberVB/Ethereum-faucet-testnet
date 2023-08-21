4446a822f9053ef5ed66f8f2985994fa1855231c
Phathdt <phathdt379@gmail.com>
2023-08-21 16:23:08 +0700

    update sample infra

--- infra/rancher.tf
+++ infra/rancher.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "rancher" {
   ami                    = "ami-091a58610910a87a9"
   instance_type          = "t3.medium"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_2.id]
   key_name               = aws_key_pair.ssh_key.key_name
-  availability_zone      = var.azs[1]
-  subnet_id              = aws_subnet.public_subnets[1].id
+  availability_zone      = module.vpc.azs[1]
+  subnet_id              = module.vpc.public_subnets[1]
 
   tags = merge(
     { Name = "rancher" },
@@ -12,7 +12,7 @@ resource "aws_instance" "rancher" {
   )
 
   root_block_device {
-    volume_size = 100
+    volume_size = 50
   }
 
 
@@ -33,6 +33,7 @@ resource "aws_instance" "rancher" {
 resource "aws_eip" "rancher-eip" {
   instance = aws_instance.rancher.id
   vpc      = true
+
   depends_on = [
     aws_instance.rancher
   ]
@@ -41,4 +42,30 @@ resource "aws_eip" "rancher-eip" {
     { Name = "eip-rancher" },
     local.tags
   )
+}
+
+resource "aws_lb_target_group" "rancher" {
+  name = "rancher-tg"
+  depends_on = [
+    aws_instance.rancher
+  ]
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+
+  tags = merge(
+    { Name = "rancher TG" },
+    local.tags
+  )
+}
+
+resource "aws_lb_target_group_attachment" "rancher" {
+  depends_on = [
+    aws_lb_target_group.rancher
+  ]
+
+  target_group_arn = aws_lb_target_group.rancher.arn
+  target_id        = aws_instance.rancher.id
+  port             = 80
 }
