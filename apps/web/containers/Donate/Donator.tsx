ec2288f884a395703b63b0b93e146175f760809d
dungle <quangdung100194@gmail.com>
2023-09-18 13:12:15 +0700

    add list donate from foundation

--- apps/web/containers/Donate/Donator.tsx
+++ apps/web/containers/Donate/Donator.tsx
@@ -11,6 +11,25 @@ import { getDonators } from '@apiService';
 import { getAppConfig } from '@config';
 import { DonateContext } from '@/contexts';
 import Link from 'next/link';
+import Image from 'next/image';
+
+const FoundationTeams = [
+  {
+    image: 'bnb',
+    name: 'BNB chain Foundation',
+    link: 'https://www.bnbchain.org/en',
+  },
+  {
+    image: 'arbitrum',
+    name: 'Arbitrum Foundation',
+    link: 'https://arbitrum.foundation/',
+  },
+  {
+    image: 'polygon',
+    name: 'Polygon Foundation',
+    link: 'https://polygon.technology/',
+  },
+];
 
 export const Donators: React.ForwardRefRenderFunction<HTMLDivElement, {}> = (props, ref) => {
   const [donators, setDonators] = useState<Donator[]>([]);
@@ -33,22 +52,62 @@ export const Donators: React.ForwardRefRenderFunction<HTMLDivElement, {}> = (pro
   useEffect(() => {
     getListDonators();
   }, [getListDonators, donateId]);
-  if (donators?.length < 1) return null;
   return (
     <Box
       sx={{
         position: 'relative',
+        backgroundColor: '#F4F4F4',
+        borderRadius: '8px',
+        padding: '24px 16px',
       }}
     >
       <Box
         component="h3"
         sx={{
-          textAlign: 'center',
           fontSize: '18px',
-          marginBottom: 0,
+          textAlign: 'center',
+          color: '#767676',
+          marginTop: 0,
         }}
       >
         DONATORS
+      </Box>
+      <Box
+        component="h5"
+        sx={{
+          fontSize: '14px',
+          fontWeight: 'regular',
+          marginTop: '24px',
+        }}
+      >
+        We extend our heartfelt appreciation to the foundation teams, for generously donating testnet tokens that
+        sustain our faucet&apos;s operation.
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '60px',
+        }}
+      >
+        {FoundationTeams.map((f) => {
+          return (
+            <Link href={f.link} target="_blank" key={f.name} aria-label={f.name}>
+              <Image width={48} height={48} src={`/assets/images/svg/${f.image}.svg`} alt={f.name} />
+            </Link>
+          );
+        })}
+      </Box>
+      <Box
+        component="h5"
+        sx={{
+          fontSize: '14px',
+          fontWeight: 'regular',
+          marginTop: '24px',
+        }}
+      >
+        We would also like to express our gratitude to the anonymous contributors whose support has been invaluable.
+        Your contributions fuel our mission and drive the growth of blockchain technology.
       </Box>
       <List sx={{ width: '100%', maxHeight: 400, overflow: 'auto' }}>
         {donators.map((donate) => {
